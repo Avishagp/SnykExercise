70614f1a2b2c61d83f94bb08ec3412d05de0e723
Avishagp <44115709+Avishagp@users.noreply.github.com>
2020-09-04 17:33:48 +0300

    Refactor into Package class that handles dependency management

--- app.py
+++ app.py
@@ -5,8 +5,45 @@ import json
 app = Flask(__name__)
 
 
+class Package:
+    CACHE = {}
+
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+        self.deps = None
+        Package.CACHE[self.name + self.version] = self
+
+    def add_dependencies(self, dependencies):
+        for name, ver in dependencies.items():
+            # "my_dep": "^1.0.0"
+            # "my_dep": "~1.0.0"
+            ver = ver.lstrip(['^', '~'])
+            pkg_name = name + ver
+
+            # Either get the package instance from the cache or create a new one
+            pkg = Package.CACHE.get(pkg_name, Package(name, ver))
+            # Add Package instance to the dependencies list
+            self.deps.append(pkg)
+
+    def discover_deps(self):
+        if self.deps is not None:
+            return
+
+        # Need to discover dependencies since it hasn't been done before
+        self.deps = []
+        response = requests.get(f"https://registry.npmjs.org/{self.name}/{self.version}")
+        pkg_info = response.json()
+
+        # dependencies appear in json under 'dependencies' OR under 'devDependencies'
+        if 'dependencies' in pkg_info:
+            self.add_dependencies(pkg_info['dependencies'])
+        elif 'devDependencies' in pkg_info:
+            self.add_dependencies(pkg_info['devDependencies'])
+
+
 #
-# Gets a list of string. Each string represents a dependency.
+# Gets a list of strings. Each string represents a dependency.
 # Resturns TODO
 #
 def create_tree(dependencies):
@@ -14,39 +51,13 @@ def create_tree(dependencies):
 
 
 #
-# Gets a dictionary that contains all data about a package.
-# Returns a list of all dependencies.
-#
-def get_dependencies(pkg_info):
-    dependencies = []
-    if 'dependencies' in pkg_info:
-        dependencies.extend(pkg_info['dependencies'])
-    elif 'devDependencies' in pkg_info:
-        dependencies.extend(pkg_info['devDependencies'])
-
-    return dependencies
-
-
-#
 # Gets a string package name and an optional version string.
 # Returns TODO
+# TODO get latest version number
 #
 @app.route('/present/<pkg_name>/<version>')
-@app.route('/present/<pkg_name>')
-def present_dependencies(pkg_name, version=None):
-    # Send a Get request using package name
-    if version:
-        response = requests.get("https://registry.npmjs.org/" + str(pkg_name) + "/" + str(version))
-    else:
-        response = requests.get("https://registry.npmjs.org/" + str(pkg_name))
-
-    # Get the entire set of dependencies
-    dependencies = get_dependencies(response.json())
-    print(dependencies)
-    # Get a tree string for presentation
-    tree = create_tree(dependencies)
-
-    # Present the tree!
+def present_dependencies(pkg_name, version):
+    pass
 
 
 if __name__ == '__main__':
